960312a718c2d96c732ed4f114b7a11d98c9630e
Diego Suárez Ramos <dsuaran@gmail.com>
2025-01-05 18:21:58 +0100

    navbar funcionando

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { Col, Container, Row } from "react-bootstrap";
 import SidebarMenu from "./components/Sidebar/SidebarMenu";
 
 function App() {
   return (
-    SidebarMenu()
+    <div style={{ display: "flex" }}>
+      <SidebarMenu />
+      <div style={{ flex: 1 }}>
+        <h1>hola</h1>
+      </div>
+    </div>
   );
 }
 
--- client/src/components/Sidebar/SidebarMenu.js
+++ client/src/components/Sidebar/SidebarMenu.js
@@ -1,100 +1,91 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Container, Row, Col, Nav, Dropdown } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import "bootstrap/js/dist/dropdown";
 const SidebarMenu = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="bg-dark col-auto col-md-3 vh-100 d-flex justify-content-between flex-column">
+    <Container fluid>
+      <Row>
+        <Col
+          className="bg-dark vh-100 d-flex justify-content-between flex-column"
+          xs="auto"
+          md={3}
+        >
           <div>
-            <a className="text-decoration-none text-white d-none d-sm-inline d-flex align-items-center ms-3 mt-3">
+            {/* Brand */}
+            <Nav.Link
+              href="#"
+              className="text-decoration-none text-white d-none d-sm-inline d-flex align-items-center ms-3 mt-3"
+            >
               <i className="fs-4 bi bi-speedometer"></i>
               <span className="ms-1 fs-4 d-none d-sm-inline">Brand</span>
-            </a>
+            </Nav.Link>
             <hr className="text-secondary d-none d-sm-block" />
-            <ul className="nav nav-pills flex-column mt-3 mt-sm-0">
-              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
-                <a
-                  href="#"
-                  className="nav-link text-white fs-5"
-                  aria-current="page"
-                >
+
+            {/* Sidebar Menu */}
+            <Nav className="nav-pills flex-column mt-3 mt-sm-0">
+              <Nav.Item className="fs-4 my-1 py-2 py-sm-0">
+                <Nav.Link href="#" className="text-white fs-5">
                   <i className="bi bi-speedometer2"></i>
                   <span className="fs-4 ms-3 d-none d-sm-inline">
                     Dashboard
                   </span>
-                </a>
-              </li>
-              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
-                <a
-                  href="#"
-                  className="nav-link text-white fs-5"
-                  aria-current="page"
-                >
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="fs-4 my-1 py-2 py-sm-0">
+                <Nav.Link href="#" className="text-white fs-5">
                   <i className="bi bi-house"></i>
                   <span className="fs-4 ms-3 d-none d-sm-inline">Home</span>
-                </a>
-              </li>
-              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
-                <a
-                  href="#"
-                  className="nav-link text-white fs-5"
-                  aria-current="page"
-                >
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="fs-4 my-1 py-2 py-sm-0">
+                <Nav.Link href="#" className="text-white fs-5">
                   <i className="bi bi-table"></i>
                   <span className="fs-4 ms-3 d-none d-sm-inline">Orders</span>
-                </a>
-              </li>
-              <li className="nav-item text-white fs-4  my-1 py-2 py-sm-0">
-                <a
-                  href="#"
-                  className="nav-link text-white fs-5"
-                  aria-current="page"
-                >
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="fs-4 my-1 py-2 py-sm-0">
+                <Nav.Link href="#" className="text-white fs-5">
                   <i className="bi bi-grid"></i>
                   <span className="fs-4 ms-3 d-none d-sm-inline">Products</span>
-                </a>
-              </li>
-              <li className="nav-item text-white fs-4 my-1 py-2 py-sm-0">
-                <a
-                  href="#"
-                  className="nav-link text-white fs-5"
-                  aria-current="page"
-                >
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="fs-4 my-1 py-2 py-sm-0">
+                <Nav.Link href="#" className="text-white fs-5">
                   <i className="bi bi-table"></i>
                   <span className="fs-4 ms-3 d-none d-sm-inline">
                     Customers
                   </span>
-                </a>
-              </li>
-            </ul>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
           </div>
-          <div class="dropdown open">
-            <a
-              class="text-decoration-none text-white dropdown-toggle p-3"
-              type="button"
-              id="triggerId"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
+
+          {/* Dropdown Menu */}
+          <Dropdown className="p-3">
+            <Dropdown.Toggle
+              variant="dark"
+              id="dropdown-basic"
+              className="text-white text-decoration-none"
             >
               <i className="bi bi-person-circle"></i>
               <span className="ms-2 d-none d-sm-inline">dsuarezr</span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="triggerId">
-              <a class="dropdown-item" href="#">
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#">
                 <span className="d-sm-inline">1</span>
                 <span className="d-none d-sm-block">Profile</span>
-              </a>
-              <a class="dropdown-item" href="#">
+              </Dropdown.Item>
+              <Dropdown.Item href="#">
                 <span className="d-sm-inline">2</span>
                 <span className="d-none d-sm-block">Settings</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
