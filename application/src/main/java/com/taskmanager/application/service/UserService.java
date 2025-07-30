fc3dfdbb31bb9aef2eb760ac478694ca4e8bcfde
Diego Suárez Ramos <dsuaran@gmail.com>
2025-07-31 01:37:48 +0200

    cositas

--- application/src/main/java/com/taskmanager/application/service/UserService.java
+++ application/src/main/java/com/taskmanager/application/service/UserService.java
@@ -12,52 +12,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    // @Autowired
-    // UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-    // public User newUser(User user){ /*Esto solo un administrador */
-    //     return userRepository.save(user);  
-    // }
+    public User newUser(User user){ /*Esto solo un administrador */
+        return userRepository.save(user);  
+    }
 
-    // public void deleteUser(User user){ /*Esto solo un administrador */
-    //     userRepository.delete(user);
-    // }
+    public void deleteUser(User user){ /*Esto solo un administrador */
+        userRepository.delete(user);
+    }
 
-    // public void deleteUserById(Long id) {
-    //     userRepository.deleteById(id);
-    // }
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
 
-    // public User findByUserId(Long id)throws ResourceNotFoundException {
+    public User findByUserId(Long id)throws ResourceNotFoundException {
 
-    //     Optional<User> user = userRepository.findById(id);
-    //     if(user.isPresent()){
-    //         return user.get();
-    //     }else{
-    //         throw new ResourceNotFoundException("User not found with id: "+id);
-    //     }
-    // }
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }else{
+            throw new ResourceNotFoundException("User not found with id: "+id);
+        }
+    }
 
-    // public List<User> getAllUser(){
-    //     return userRepository.findAll();
-    // }
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
 
-    // public User findUsersByUserName(String name) throws ResourceNotFoundException {
-    //     Optional<User> user = userRepository.findByUsername(name);
-    //     if(user.isPresent()){
-    //         return user.get();
-    //     }else{
-    //         throw new ResourceNotFoundException("User not found with name: "+name);
-    //     }
-    // }
+    public User findUsersByUserName(String name) throws ResourceNotFoundException {
+        Optional<User> user = userRepository.findByUsername(name);
+        if(user.isPresent()){
+            return user.get();
+        }else{
+            throw new ResourceNotFoundException("User not found with name: "+name);
+        }
+    }
 
-    // public User findByEmail(String email) throws ResourceNotFoundException {
-    //     Optional<User> user = userRepository.findByEmail(email);
-    //     if(user.isPresent()){
-    //         return user.get();
-    //     }else{
-    //         throw new ResourceNotFoundException("User not found with email: "+email);
-    //     }
-    // }
+    public User findByEmail(String email) throws ResourceNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+        if(user.isPresent()){
+            return user.get();
+        }else{
+            throw new ResourceNotFoundException("User not found with email: "+email);
+        }
+    }
 
     
 }
